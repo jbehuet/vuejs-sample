77292f5faf786430c55bf6741e3e60b2bac1d34e
jbehuet <jbehuet@gmail.com>
2016-02-06 00:34:13 +0100

    Change API KEY

--- frontend/js/mixins/weather.js
+++ frontend/js/mixins/weather.js
@@ -1,7 +1,9 @@
 var weather = {
+    
   currentWeather: function(){
-       return Vue.http.get('http://api.openweathermap.org/data/2.5/weather?q=La%20loupe&units=metric&appid=2de143494c0b295cca9337e1e96b00e0');
+       return Vue.http.get('http://api.openweathermap.org/data/2.5/weather?q=La%20loupe&units=metric&appid=44db6a862fba0b067b1930da0d769e98');
   }
+    
 };
 
 module.exports = weather;
